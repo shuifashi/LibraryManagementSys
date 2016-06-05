85d4313ece1132af450e95fa497f9c4ddade5745
StargazerYi <yichuanxiong@126.com>
2016-06-05 12:05:52 +0800

    booking&manage
    booking&manage

---
+++ WebContent/WebPage/booking/js/booking.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	if($("div.successFlag").html()=="0" || $("div.successFlag").html()=="1") {
+		if($("div.successFlag").html()=="0") {
+			$("#outputWords").html("预约失败")
+		} else {
+			$("#outputWords").html("预约成功")
+		}
+		$(function () { $('#myModal').modal({
+	      keyboard: true
+	   	})});
+	}
+})
